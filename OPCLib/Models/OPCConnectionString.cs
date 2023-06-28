017db8699f8cbbc892b01a9119b67f128019c603
chandresekaran-bauercontrols <chandresekaran@bauercontrols.com>
2023-06-28 16:59:20 -0400

    Inital Commit

---
+++ OPCLib/Models/OPCConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCLib.Models
+{
+   public class OPCConnectionString
+   {
+      public string URL { get; set; }
+      public string Name { get; set; }
+      public string Password { get; set; }
+   }
+}
---
+++ OPCLib/Service/IOPCClient.cs
@@ -0,0 +1,20 @@
+﻿using OPCLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPCLib.Service
+{
+   public interface IOPCClient
+   {
+      event EventHandler<TagValueChanged> TagValueChanged;
+      void OpenConnection(OPCConnectionString connectionString);
+      void CloseConnection();
+      IEnumerable<OPCTag> GetAllTags();
+      IEnumerable<Dictionary<string,object>> GetTagValues(IEnumerable<string> tags);
+      void WriteTagValue(string tag, object value);
+      void SubscribeValueChange(string tag);
+   }
+}
